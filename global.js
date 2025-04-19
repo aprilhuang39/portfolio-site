8598acae40473658a90475dfde13bdb7386f87f7
aprilhuang39 <aprilrouhuang@gmail.com>
2025-04-18 23:24:21 -0700

    added js

---
+++ global.js
@@ -0,0 +1,64 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+    { url: '', title: 'Home'},
+    { url: 'projects/', title: 'Projects'},
+    { url: 'cv/', title: 'Resume'},
+    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/aprilhuang39/', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    const at_home = document.documentElement.classList.contains('home');
+    let url = p.url;    
+    if (!at_home && !url.startsWith('http')) {
+        url = '../' + url;
+    }
+    let title = p.title;
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+    nav.append(a);
+}  
+
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+        <label class="color-scheme">
+            Theme:
+            <select>
+                <option value="light dark">Automatic</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+            </select>
+        </label>`
+);
+
+let select = document.querySelector('select');
+
+if ('colorScheme' in localStorage) {
+    let scheme = localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', scheme);
+    select.value = scheme;
+}
+
+select.addEventListener('input', function (event) {
+    let scheme = event.target.value;
+    document.documentElement.style.setProperty('color-scheme', scheme);
+    localStorage.colorScheme = scheme;
+});
